71c22cc972b133891e6e0290d7b26db4e6e84c00
Slava Dobrinets <thedab97@gmail.com>
2023-08-21 21:44:37 +0300

    first commit

---
+++ components/site-header.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+import { Icons } from "@/components/icons"
+import { MainNav } from "@/components/main-nav"
+import { MobileNav } from "@/components/mobile-nav"
+import { CommandMenu } from "@/components/command-menu"
+import { ThemeToggle } from "@/components/theme-toggle"
+
+export function SiteHeader() {
+  return (
+    <header className="bg-background sticky top-0 z-40 w-full border-b">
+      <div className="container flex h-16 items-center space-x-0 sm:justify-between sm:space-x-0">
+        <MainNav items={siteConfig.mainNav} />
+        <MobileNav items={siteConfig.mainNav} />
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <div className="w-full flex-1 md:w-auto md:flex-none">
+            <CommandMenu />
+          </div>
+          <nav className="flex items-center space-x-1">
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={buttonVariants({
+                  size: "full",
+                  variant: "ghost",
+                })}
+              >
+                <Icons.gitHub className="h-5 w-5" />
+                <span className="sr-only">GitHub</span>
+              </div>
+            </Link>
+            <Link
+              href={siteConfig.links.twitter}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={buttonVariants({                  
+                  size: "full",
+                  variant: "ghost",
+                })}
+              >
+                <Icons.twitter className="h-5 w-5 fill-current" />
+                <span className="sr-only">Twitter</span>
+              </div>
+            </Link>
+            <ThemeToggle />
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
---
+++ config/site.ts
@@ -0,0 +1,23 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Next.js",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  icon: "/public/favicon.ico",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Table",
+      href: "/table",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn/ui",
+    docs: "https://ui.shadcn.com",
+  },
+}
