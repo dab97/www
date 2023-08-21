eeb1852035bb94ffe4910a17ea0f995e8770cb94
Slava Dobrinets <thedab97@gmail.com>
2023-08-22 02:23:19 +0300

    Added DataTable Page

--- app/table/page.tsx
+++ app/table/page.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link"
-
+import { DataTable } from "@/components/datatable"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 
 export default function IndexPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+    <section className="container grid items-center justify-center gap-4 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[1280px] flex-col items-center gap-2">
+        <h1 className="text-center text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Страница Таблицы с данными
-        </h1>        
+        </h1>
+      <div className="container flex flex-col items-center gap-2">
+        < DataTable/>
+      </div>        
       </div>
     </section>
   )
