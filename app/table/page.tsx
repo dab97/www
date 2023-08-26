31f3c84316b122891683ee9f42fe9f01b4f132f4
Slava Dobrinets <thedab97@gmail.com>
2023-08-26 16:29:54 +0300

    upd

--- app/table/page.tsx
+++ app/table/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { DataTable } from "@/components/datatable"
+// import { DataTable } from "@/components/datatable"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 
@@ -10,9 +10,9 @@ export default function IndexPage() {
         <h1 className="text-center text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Страница Таблицы с данными
         </h1>
-      <div className="container flex flex-col items-center gap-2">
+      {/* <div className="container flex flex-col items-center gap-2">
         < DataTable/>
-      </div>        
+      </div>         */}
       </div>
     </section>
   )
